3441ac516e6fd90582c687e92d33c7b89148929e
Gene Boggs <gene@ology.net>
2023-05-28 09:05:22 -0700

    Move adapted book examples to KBPFMR

--- KBPFMR.pro
+++ KBPFMR.pro
@@ -1,3 +1,121 @@
+%% Adapted from the book, Knowledge-Based Programming for Music Research by Schaffer & McGee
+
+/**        base, key, note, chord, function */
+my_key_member(c,    maj, c,    maj,   tonic).
+my_key_member(c,    maj, d,    min,   supertonic).
+my_key_member(c,    maj, e,    min,   mediant).
+my_key_member(c,    maj, f,    maj,   subdominant).
+my_key_member(c,    maj, g,    maj,   dominant).
+my_key_member(c,    maj, a,    min,   submediant).
+my_key_member(c,    maj, b,    dim,   leading_tone).
+% minor
+my_key_member(c,    min, c,    min,   tonic).
+my_key_member(c,    min, d,    dim,   supertonic).
+my_key_member(c,    min, ef,   maj,   mediant).
+my_key_member(c,    min, f,    min,   subdominant).
+my_key_member(c,    min, g,    min,   dominant).
+my_key_member(c,    min, af,   maj,   submediant).
+my_key_member(c,    min, bf,   maj,   subtonic).
+
+my_key_member(d,    maj, d,    maj,   tonic).
+my_key_member(d,    maj, e,    min,   supertonic).
+my_key_member(d,    maj, gf,   min,   mediant).
+my_key_member(d,    maj, g,    maj,   subdominant).
+my_key_member(d,    maj, a,    maj,   dominant).
+my_key_member(d,    maj, b,    min,   submediant).
+my_key_member(d,    maj, df,   dim,   leading_tone).
+% minor
+my_key_member(d,    min, d,    min,   tonic).
+my_key_member(d,    min, e,    dim,   supertonic).
+my_key_member(d,    min, f,    maj,   mediant).
+my_key_member(d,    min, g,    min,   subdominant).
+my_key_member(d,    min, a,    min,   dominant).
+my_key_member(d,    min, bf,   maj,   submediant).
+my_key_member(d,    min, c,    maj,   subtonic).
+
+my_key_member(e,    maj, e,    maj,   tonic).
+my_key_member(e,    maj, gf,   min,   supertonic).
+my_key_member(e,    maj, af,   min,   mediant).
+my_key_member(e,    maj, a,    maj,   subdominant).
+my_key_member(e,    maj, b,    maj,   dominant).
+my_key_member(e,    maj, df,   min,   submediant).
+my_key_member(e,    maj, ef,   dim,   leading_tone).
+% minor
+my_key_member(e,    min, e,    min,   tonic).
+my_key_member(e,    min, gf,   dim,   supertonic).
+my_key_member(e,    min, g,    maj,   mediant).
+my_key_member(e,    min, a,    min,   subdominant).
+my_key_member(e,    min, b,    min,   dominant).
+my_key_member(e,    min, c,    maj,   submediant).
+my_key_member(e,    min, d,    maj,   subtonic).
+
+my_key_member(f,    maj, f,    maj,   tonic).
+my_key_member(f,    maj, g,    min,   supertonic).
+my_key_member(f,    maj, a,    min,   mediant).
+my_key_member(f,    maj, bf,   maj,   subdominant).
+my_key_member(f,    maj, c,    maj,   dominant).
+my_key_member(f,    maj, d,    min,   submediant).
+my_key_member(f,    maj, e,    dim,   leading_tone).
+% minor
+my_key_member(f,    min, f,    min,   tonic).
+my_key_member(f,    min, g,    dim,   supertonic).
+my_key_member(f,    min, af,   maj,   mediant).
+my_key_member(f,    min, bf,   min,   subdominant).
+my_key_member(f,    min, c,    min,   dominant).
+my_key_member(f,    min, df,   maj,   submediant).
+my_key_member(f,    min, ef,   maj,   subtonic).
+
+my_key_member(g,    maj, g,    maj,   tonic).
+my_key_member(g,    maj, a,    min,   supertonic).
+my_key_member(g,    maj, b,    min,   mediant).
+my_key_member(g,    maj, c,    maj,   subdominant).
+my_key_member(g,    maj, d,    maj,   dominant).
+my_key_member(g,    maj, e,    min,   submediant).
+my_key_member(g,    maj, gf,   dim,   leading_tone).
+% minor
+my_key_member(g,    min, g,    min,   tonic).
+my_key_member(g,    min, a,    dim,   supertonic).
+my_key_member(g,    min, bf,   maj,   mediant).
+my_key_member(g,    min, c,    min,   subdominant).
+my_key_member(g,    min, d,    min,   dominant).
+my_key_member(g,    min, ef,   maj,   submediant).
+my_key_member(g,    min, f,    maj,   subtonic).
+
+my_key_member(a,    maj, a,    maj,   tonic).
+my_key_member(a,    maj, b,    min,   supertonic).
+my_key_member(a,    maj, df,   min,   mediant).
+my_key_member(a,    maj, d,    maj,   subdominant).
+my_key_member(a,    maj, e,    maj,   dominant).
+my_key_member(a,    maj, gf,   min,   submediant).
+my_key_member(a,    maj, af,   dim,   leading_tone).
+% minor
+my_key_member(a,    min, a,    min,   tonic).
+my_key_member(a,    min, b,    dim,   supertonic).
+my_key_member(a,    min, c,    maj,   mediant).
+my_key_member(a,    min, d,    min,   subdominant).
+my_key_member(a,    min, e,    min,   dominant).
+my_key_member(a,    min, f,    maj,   submediant).
+my_key_member(a,    min, g,    maj,   subtonic).
+
+my_key_member(b,    maj, b,    maj,   tonic).
+my_key_member(b,    maj, df,   min,   supertonic).
+my_key_member(b,    maj, ef,   min,   mediant).
+my_key_member(b,    maj, e,    maj,   subdominant).
+my_key_member(b,    maj, gf,   maj,   dominant).
+my_key_member(b,    maj, af,   min,   submediant).
+my_key_member(b,    maj, bf,   dim,   leading_tone).
+% minor
+my_key_member(b,    min, b,    min,   tonic).
+my_key_member(b,    min, df,   dim,   supertonic).
+my_key_member(b,    min, d,    maj,   mediant).
+my_key_member(b,    min, e,    min,   subdominant).
+my_key_member(b,    min, gf,   min,   dominant).
+my_key_member(b,    min, g,    maj,   submediant).
+my_key_member(b,    min, a,    maj,   subtonic).
+
+
+% Taken directly from the book:
+
 key_member(cmaj,cmaj,tonic).
 key_member(cmaj,cmin,tonic).
 key_member(cmaj,gmaj,dominant).
--- music.pl
+++ music.pl
@@ -1,118 +1,3 @@
-%% Adapted from the book, Knowledge-Based Programming for Music Research by Schaffer & McGee
-
-/**        base, key, note, chord, function */
-key_member(c,    maj, c,    maj,   tonic).
-key_member(c,    maj, d,    min,   supertonic).
-key_member(c,    maj, e,    min,   mediant).
-key_member(c,    maj, f,    maj,   subdominant).
-key_member(c,    maj, g,    maj,   dominant).
-key_member(c,    maj, a,    min,   submediant).
-key_member(c,    maj, b,    dim,   leading_tone).
-% minor
-key_member(c,    min, c,    min,   tonic).
-key_member(c,    min, d,    dim,   supertonic).
-key_member(c,    min, ef,   maj,   mediant).
-key_member(c,    min, f,    min,   subdominant).
-key_member(c,    min, g,    min,   dominant).
-key_member(c,    min, af,   maj,   submediant).
-key_member(c,    min, bf,   maj,   subtonic).
-
-key_member(d,    maj, d,    maj,   tonic).
-key_member(d,    maj, e,    min,   supertonic).
-key_member(d,    maj, gf,   min,   mediant).
-key_member(d,    maj, g,    maj,   subdominant).
-key_member(d,    maj, a,    maj,   dominant).
-key_member(d,    maj, b,    min,   submediant).
-key_member(d,    maj, df,   dim,   leading_tone).
-% minor
-key_member(d,    min, d,    min,   tonic).
-key_member(d,    min, e,    dim,   supertonic).
-key_member(d,    min, f,    maj,   mediant).
-key_member(d,    min, g,    min,   subdominant).
-key_member(d,    min, a,    min,   dominant).
-key_member(d,    min, bf,   maj,   submediant).
-key_member(d,    min, c,    maj,   subtonic).
-
-key_member(e,    maj, e,    maj,   tonic).
-key_member(e,    maj, gf,   min,   supertonic).
-key_member(e,    maj, af,   min,   mediant).
-key_member(e,    maj, a,    maj,   subdominant).
-key_member(e,    maj, b,    maj,   dominant).
-key_member(e,    maj, df,   min,   submediant).
-key_member(e,    maj, ef,   dim,   leading_tone).
-% minor
-key_member(e,    min, e,    min,   tonic).
-key_member(e,    min, gf,   dim,   supertonic).
-key_member(e,    min, g,    maj,   mediant).
-key_member(e,    min, a,    min,   subdominant).
-key_member(e,    min, b,    min,   dominant).
-key_member(e,    min, c,    maj,   submediant).
-key_member(e,    min, d,    maj,   subtonic).
-
-key_member(f,    maj, f,    maj,   tonic).
-key_member(f,    maj, g,    min,   supertonic).
-key_member(f,    maj, a,    min,   mediant).
-key_member(f,    maj, bf,   maj,   subdominant).
-key_member(f,    maj, c,    maj,   dominant).
-key_member(f,    maj, d,    min,   submediant).
-key_member(f,    maj, e,    dim,   leading_tone).
-% minor
-key_member(f,    min, f,    min,   tonic).
-key_member(f,    min, g,    dim,   supertonic).
-key_member(f,    min, af,   maj,   mediant).
-key_member(f,    min, bf,   min,   subdominant).
-key_member(f,    min, c,    min,   dominant).
-key_member(f,    min, df,   maj,   submediant).
-key_member(f,    min, ef,   maj,   subtonic).
-
-key_member(g,    maj, g,    maj,   tonic).
-key_member(g,    maj, a,    min,   supertonic).
-key_member(g,    maj, b,    min,   mediant).
-key_member(g,    maj, c,    maj,   subdominant).
-key_member(g,    maj, d,    maj,   dominant).
-key_member(g,    maj, e,    min,   submediant).
-key_member(g,    maj, gf,   dim,   leading_tone).
-% minor
-key_member(g,    min, g,    min,   tonic).
-key_member(g,    min, a,    dim,   supertonic).
-key_member(g,    min, bf,   maj,   mediant).
-key_member(g,    min, c,    min,   subdominant).
-key_member(g,    min, d,    min,   dominant).
-key_member(g,    min, ef,   maj,   submediant).
-key_member(g,    min, f,    maj,   subtonic).
-
-key_member(a,    maj, a,    maj,   tonic).
-key_member(a,    maj, b,    min,   supertonic).
-key_member(a,    maj, df,   min,   mediant).
-key_member(a,    maj, d,    maj,   subdominant).
-key_member(a,    maj, e,    maj,   dominant).
-key_member(a,    maj, gf,   min,   submediant).
-key_member(a,    maj, af,   dim,   leading_tone).
-% minor
-key_member(a,    min, a,    min,   tonic).
-key_member(a,    min, b,    dim,   supertonic).
-key_member(a,    min, c,    maj,   mediant).
-key_member(a,    min, d,    min,   subdominant).
-key_member(a,    min, e,    min,   dominant).
-key_member(a,    min, f,    maj,   submediant).
-key_member(a,    min, g,    maj,   subtonic).
-
-key_member(b,    maj, b,    maj,   tonic).
-key_member(b,    maj, df,   min,   supertonic).
-key_member(b,    maj, ef,   min,   mediant).
-key_member(b,    maj, e,    maj,   subdominant).
-key_member(b,    maj, gf,   maj,   dominant).
-key_member(b,    maj, af,   min,   submediant).
-key_member(b,    maj, bf,   dim,   leading_tone).
-% minor
-key_member(b,    min, b,    min,   tonic).
-key_member(b,    min, df,   dim,   supertonic).
-key_member(b,    min, d,    maj,   mediant).
-key_member(b,    min, e,    min,   subdominant).
-key_member(b,    min, gf,   min,   dominant).
-key_member(b,    min, g,    maj,   submediant).
-key_member(b,    min, a,    maj,   subtonic).
-
 mode(maj, [
     meta{ chord:maj, roman:r_I,   function:tonic },
     meta{ chord:min, roman:r_ii,  function:supertonic },
@@ -198,18 +83,34 @@ get_mode(Result, Note, Key, Mode) :-
     nth0(Idx, Meta, Dict),
     Result = [Base, Mode, Note, Dict].
 
-scale(c, maj, [ c, d,  e,  f,  g,  a,  b  ]).
-scale(c, min, [ c, d,  ef, f,  g,  af, bf ]).
-scale(d, maj, [ d, e,  gf, g,  a,  b,  df ]).
+% C
+scale(c, maj,        [ c, d,  e,  f,  g,  a,  b  ]).
+scale(c, ionian,     [ c, d,  e,  f,  g,  a,  b  ]).
+scale(c, dorian,     [ c, d,  ef, f,  g,  a,  bf ]).
+scale(c, phrygian,   [ c, df, ef, f,  g,  af, bf ]).
+scale(c, lydian,     [ c, d,  e,  gf, g,  a,  b  ]).
+scale(c, mixolydian, [ c, d,  e,  f,  g,  a,  bf ]).
+scale(c, min,        [ c, d,  ef, f,  g,  af, bf ]).
+scale(c, aeolean,    [ c, d,  ef, f,  g,  af, bf ]).
+scale(c, locrian,    [ c, df, ef, f,  gf, af, bf ]).
+% D
+scale(d, maj,        [ d, e,  gf, g,  a,  b,  df ]).
+scale(d, ionian,     [ d, e,  gf, g,  a,  b,  df ]).
+scale(d, lydian,     [ d, e,  gf, g,  a,  b,  df ]).
 scale(d, min, [ d, e,  f,  g,  a,  bf, c  ]).
+% D
 scale(e, maj, [ e, gf, af, a,  b,  df, ef ]).
 scale(e, min, [ e, gf, g,  a,  b,  c,  d  ]).
+% D
 scale(f, maj, [ f, g,  a,  bf, c,  d,  e  ]).
 scale(f, min, [ f, g,  af, bf, c,  df, ef ]).
+% D
 scale(g, maj, [ g, a,  b,  c,  d,  e,  gf ]).
 scale(g, min, [ g, a,  bf, c,  d,  ef, f  ]).
+% D
 scale(a, maj, [ a, b,  df, d,  e,  gf, af ]).
 scale(a, min, [ a, b,  c,  d,  e,  f,  g  ]).
+% D
 scale(b, maj, [ b, df, ef, e,  gf, af, bf ]).
 scale(b, min, [ b, df, d,  e,  gf, g,  a  ]).
 
